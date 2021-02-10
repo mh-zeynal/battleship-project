5e693d9eb8954b998bed3f506bb8b1b4990f16f8
mh-zeynal <78679020+mh-zeynal@users.noreply.github.com>
2021-02-10 23:39:12 +0330

    nothing important
    it has bugs so nothing important has been changed

--- Linked_list_stuff.h
+++ Linked_list_stuff.h
@@ -6,17 +6,27 @@ typedef struct ship{
     int end_y;
     struct ship* next;
 }ship;
+
 void append(ship **head , int i , int j , int k , int h , int z) {
     ship *current = (ship *) malloc(sizeof(ship));
-    current->start_x = i;
-    current->start_y = j;
-    current->end_x = k;
-    current->end_y = h;
-    current->ship_id = z;
-    ship*last_node = *head;
-    //*head = current;
-    for(last_node;last_node->next != NULL; last_node = last_node->next);
-    last_node->next = current;
+    if((*head)->ship_id == -1){
+        (*head)->start_x = i;
+        (*head)->start_y = j;
+        (*head)->end_x = k;
+        (*head)->end_y = h;
+        (*head)->ship_id = z;
+    }
+    else{
+        current->start_x = i;
+        current->start_y = j;
+        current->end_x = k;
+        current->end_y = h;
+        current->ship_id = z;
+        ship*last_node = *head;
+        for(last_node;last_node->next != NULL; last_node = last_node->next);
+        last_node->next = current;
+    }
+
 }
 void delete(ship** head_ref , int key){
     ship *current = *head_ref, *prev;
