aacd2e55a4626ce2206cd424d6150968128e7993
mh-zeynal <78679020+mh-zeynal@users.noreply.github.com>
2021-02-10 23:41:43 +0330

    some changes for colorizing

--- printmap.h
+++ printmap.h
@@ -1,15 +1,41 @@
 void print_array(int size,int map[size][size]){
-    printf("\t");
+    printf("   ");
     int x=65;
     for(int i=1;i<=size;i++){
-        printf("%d\t",i);
+        setcolor(6);
+        printf("%d  ",i);
     }
     printf("\n");
     int counter=0;
     while(counter<size){
-        printf("%c\t",x);
+        setcolor(6);
+        printf("%c  ", x);
         for(int n=0;n<size;n++){
-            printf("%d\t",map[counter][n]);
+            if(map[counter][n] > 0){
+                if(map[counter][n] >= 10){
+                    setcolor(10);
+                    printf("%d ",map[counter][n]);
+                } else{
+                    setcolor(10);
+                    printf("%d  ",map[counter][n]);
+                }
+            }
+            else if (map[counter][n] == 0){
+                setcolor(3);
+                printf("%d  ",map[counter][n]);
+            }
+            else if (map[counter][n] == -3){
+                setcolor(1);
+                printf("%d  ",map[counter][n]);
+            }
+            else if (map[counter][n] == -1){
+                setcolor(12);
+                printf("%d  ",map[counter][n]);
+            }
+            else if (map[counter][n] == -2){
+                setcolor(5);
+                printf("%d  ",map[counter][n]);
+            }
         }
         counter++;
         x++;
@@ -17,23 +43,36 @@ void print_array(int size,int map[size][size]){
     }
 }
 void print_array_2(int size,int map[size][size]){
-    printf("\t");
+    printf("   ");
     int x=65;
     for(int i=1;i<=size;i++){
-        printf("%d\t",i);
+        setcolor(6);
+        printf("%d  ",i);
     }
     printf("\n");
     for(int i=0;i<size;i++){
-        printf("%c\t", x);
+        setcolor(6);
+        printf("%c  ", x);
         for(int j=0;j<size;j++){
-            if(map[i][j] >= 0)
-                printf(".\t");
-            else if (map[i][j] == -2) //full destroyed ship
-                printf("%c\t",'C');
-            else if(map[i][j] == -1) //destroyed part
-                printf("%c\t",'E');
-            else if (map[i][j] == -3) //water
-                printf("%c\t",'W');
+            if(map[i][j] >= 0){
+                setcolor(3);
+                printf(".  ");
+            }
+            else if (map[i][j] == -2){
+                //full destroyed ship
+                setcolor(5);
+                printf("%c  ",'C');
+            }
+            else if(map[i][j] == -1){
+                //destroyed part
+                setcolor(12);
+                printf("%c  ",'E');
+            }
+            else if (map[i][j] == -3){
+                //water
+                setcolor(1);
+                printf("%c  ",'W');
+            }
         }
         printf("\n");
         x++;
