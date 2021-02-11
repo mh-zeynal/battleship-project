35d43b4ab66c33defab1bfabf3c43912d71aab09
mh-zeynal <78679020+mh-zeynal@users.noreply.github.com>
2021-02-12 00:17:34 +0330

    big mistake
    The append function had a terrible mistake an a friend of mine helped me to debug it

--- Linked_list_stuff.h
+++ Linked_list_stuff.h
@@ -6,7 +6,7 @@ typedef struct ship{
     int end_y;
     struct ship* next;
 }ship;
-
+ship* lastnode;
 void append(ship **head , int i , int j , int k , int h , int z) {
     ship *current = (ship *) malloc(sizeof(ship));
     if((*head)->ship_id == -1){
@@ -15,6 +15,7 @@ void append(ship **head , int i , int j , int k , int h , int z) {
         (*head)->end_x = k;
         (*head)->end_y = h;
         (*head)->ship_id = z;
+        lastnode = *head;
     }
     else{
         current->start_x = i;
@@ -22,17 +23,7 @@ void append(ship **head , int i , int j , int k , int h , int z) {
         current->end_x = k;
         current->end_y = h;
         current->ship_id = z;
-        ship*last_node = *head;
-        for(last_node;last_node->next != NULL; last_node = last_node->next);
-        last_node->next = current;
+        lastnode->next = current;
+        lastnode = current;
     }
-
-}
-void delete(ship** head_ref , int key){
-    ship *current = *head_ref, *prev;
-    for(current;current->ship_id != key;current=current->next){
-        prev = current;
-    }
-    prev->next = current->next;
-    free(current);
 }
