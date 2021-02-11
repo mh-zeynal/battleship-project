0cdf42f06600cd57bc8245fa34cbce41b8825419
mh-zeynal <78679020+mh-zeynal@users.noreply.github.com>
2021-02-12 00:13:32 +0330

    debug loop
    one of the loops had a problem in ending....so some other condotions has been added for that problem

--- map_making.h
+++ map_making.h
@@ -4,8 +4,10 @@ void create_map(int size1 , int map[size1][size1],int num, ship* head){
     char x_1;
     int* arr = ship_kind;
     while(count <= num){
+        system("cls") ;
         setcolor(14);
         printf("Enter the first coordinates of a ship with the length of %d\n", arr[count-1]);
+        getchar();
         scanf(" %c",&x_1);
         x1 = (int)(x_1 - 'A');
         scanf("%d",&y1);
@@ -16,12 +18,13 @@ void create_map(int size1 , int map[size1][size1],int num, ship* head){
         }
         else{
             setcolor(14) ;
-            printf("choose the mode of your ship\n1)horizontal\n2)vertical\n") ;
+            printf("Choose the mode of your ship\n1)horizontal\n2)vertical\n") ;
             int mode ;
             scanf("%d", &mode) ;
             if(mode == 1){
+                system("cls") ;
                 setcolor(14);
-                printf("1)left\n2)right\n") ;
+                printf("1- left\n2- right\n") ;
                 int direction1 ;
                 scanf("%d", &direction1) ;
                 if(direction1 == 1){
@@ -33,8 +36,9 @@ void create_map(int size1 , int map[size1][size1],int num, ship* head){
                 }
             }
             else if (mode == 2){
+                system("cls") ;
                 setcolor(14);
-                printf("1)Up\n2)Down\n");
+                printf("1- Up\n2- Down\n");
                 int direction2;
                 scanf("%d", &direction2);
                 if(direction2 == 1){
@@ -58,70 +62,60 @@ void create_map(int size1 , int map[size1][size1],int num, ship* head){
                 for(int j=y1-1;j<=y2+1;j++){
                     if(check(i, i, j, j, size1) == true){
                         if(map[i][j] != 0){
+                            system("cls") ;
                             setcolor(14);
-                            printf("try again\n");
+                            printf("Try again\n");
                             flag = true;
-                            t = false;
-                            break;
+                            t = false ;
+                            break ;
                         }
                     }
                 }
                 if(flag == true){
-                    break;
+                    break ;
                 }
             }
         }
         else{
-            setcolor(14);
-            print_array(size1,map);
+            setcolor(14) ;
+            print_array(size1,map) ;
             continue;
         }
         if(t == true){
             if(x1 == x2){
-                for(int i=y1;i<=y2;i++){
-                    map[x1][i] = count;
+                for(int i = y1; i <= y2; i++){
+                    map[x1][i] = count ;
                 }
             }
             else if (y1 == y2){
-                for(int i=x1;i<=x2;i++){
-                    map[i][y1] = count;
+                for(int i = x1; i <= x2; i++){
+                    map[i][y1] = count ;
                 }
             }
-            ship* current = head;
-            printf("\n");
             append(&head, x1, y1, x2, y2, count) ;
-            printf("\n");
             count++ ;
         }
+
         print_array(size1, map) ;
     }
 }
 void map_bot(int size1 , int map[size1][size1],int num, ship* head){
     int count  = 1 ;
     int x1,y1,x2,y2;
-
     int* arr = ship_kind;
     while(count <= num){
-
-
         x1 = rand() % size1;
         y1 = rand() % size1;
-        /*if(arr[count - 1] == 1){
+        if(arr[count - 1] == 1){
             x2 = x1 ;
             y2 = y1 ;
         }
         else{
-            //setcolor(14) ;
-            //printf("choose the mode of your ship\n1)horizontal\n2)vertical\n") ;
             int mode ;
             mode = rand() % 2 + 1 ;
-            //scanf("%d", &mode) ;
             if(mode == 1){
-                //setcolor(14);
-                //printf("1)left\n2)right\n") ;
                 int direction1 ;
                 direction1 = rand() % 2 + 1 ;
-                //scanf("%d", &direction1) ;
                 if(direction1 == 1){
                     y2 = y1 - arr[count-1] + 1 ;
                     x2 = x1;
@@ -132,10 +126,8 @@ void map_bot(int size1 , int map[size1][size1],int num, ship* head){
             }
             else if (mode == 2){
                 setcolor(14);
-                //printf("1)Up\n2)Down\n");
                 int direction2;
                 direction2 = rand() % 2 + 1 ;
-                //scanf("%d", &direction2);
                 if(direction2 == 1){
                     x2 = x1 - arr[count-1] + 1 ;
                     y2 = y1;
@@ -144,15 +136,12 @@ void map_bot(int size1 , int map[size1][size1],int num, ship* head){
                     y2 = y1;
                 }
             }
-        }*/
-        int mode ;
-        mode = rand() % 2 + 1 ;
-        if(mode == 1){
-            y2 = y1 + arr[count-1] - 1;
-            x2 = x1;
-        } else if (mode == 2){
-            x2 = x1 + arr[count-1] - 1;
-            y2 = y1;
+        }
+        if(x1 > x2){
+            swap(&x1,&x2);
+        }
+        else if(y1 > y2){
+            swap(&y1,&y2);
         }
         bool flag = false , T = true;
         if(check(x1,x2,y1,y2,size1) == true){
